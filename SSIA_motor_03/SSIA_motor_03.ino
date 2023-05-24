e1e4bce5e6a6d9f83afe96db459ef4860d15f3a2
Matheus Barros Oliveira <matheusbarros.o@outlook.com>
2023-05-23 22:00:11 -0300

    23052023
    Inclusão da leitura do LCD

--- SSIA_motor_03/SSIA_motor_03.ino
+++ SSIA_motor_03/SSIA_motor_03.ino
@@ -1,3 +1,6 @@
+#include <LiquidCrystal.h>
+
+
 //definir os pinos como entradas e saídas e configurar
 #define pinCHA 2
 #define pinCHB 3
@@ -8,6 +11,10 @@ volatile long int posicao=0;
 bool teste1 = true;
 bool teste2 = true;
 int leitA,leitB;
+
+const int rs = 12, en = 11, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
+int valorMapeado, valorPotenciometro;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void interrupt_pinCHA()
 {
@@ -45,6 +52,7 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
+  lcd.begin(16, 2);
 
   pinMode(pinCHA, INPUT);
   pinMode(pinCHB, INPUT);
@@ -81,26 +89,39 @@ void loop() {
   if (teste2 == true) {
 
     for(int i=1;i<=200;i++)
-  {
-     analogWrite(pinPWM1, 0);
-     analogWrite(pinPWM2, 127);
-     Serial.print(127);Serial.println("\n");
-     Serial.println(posicao);    
-     posicao=0;
-     delay(5);
+  { 
+    valorPotenciometro = analogRead(A0); // Leitura do valor do potenciômetro
+    valorMapeado = map(valorPotenciometro, 0, 1023, 0, 255); // Mapeia o valor para o intervalo de 0 a 255
+
+    analogWrite(pinPWM1, 0);
+    analogWrite(pinPWM2, 127);
+    Serial.print(127);Serial.println("\n");
+    Serial.println(posicao);    
+     
+    lcd.clear(); // Limpa o conteúdo do display
+
+    // Escreve o valor da variável no display
+    lcd.setCursor(0, 0);
+    lcd.print("Veloc. Atual: ");
+    lcd.print(posicao);
+    lcd.setCursor(0, 1);
+    lcd.print("Set Point: ");
+    lcd.print(valorMapeado);
+    posicao=0;
+    delay(5);
   }
 
 
   }
   
-  teste1 = false;
-  teste2 = false;
+  teste1 = true;
+  teste2 = true;
 
   analogWrite(pinPWM1, 0); // (0 a 255)
   analogWrite(pinPWM2, 0);
 
-  delay(200);
 
-  return;
+
+  delay(1000);
   
 }
